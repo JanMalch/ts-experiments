708c28cae08f4054a51a9424b9c4950eb487c33d
Jan Malchert <25508038+JanMalch@users.noreply.github.com>
2020-08-05 14:56:59 +0200

    refactor: improve toms

--- src/collections/operations/typed-object-methods.ts
+++ src/collections/operations/typed-object-methods.ts
@@ -1,19 +1,20 @@
 /*
  * typed versions of Object.entries, Object.keys and Object.values
+ * See also [why TS doesn't do it this way](https://github.com/Microsoft/TypeScript/issues/12870#issuecomment-266637861).
  */
 
-export function objectEntries<T, K extends keyof T = keyof T>(
+export function objectEntries<T>(
   value: T
-): Array<[K, T[K]]> {
+): Array<[keyof T, T[keyof T]]> {
   return Object.entries(value) as any;
 }
 
-export function objectKeys<T, K extends keyof T = keyof T>(value: T): K[] {
+export function objectKeys<T>(value: T): Array<keyof T> {
   return Object.keys(value) as any;
 }
 
-export function objectValues<T, K extends keyof T = keyof T>(
+export function objectValues<T>(
   value: T
-): Array<T[K]> {
+): Array<T[keyof T]> {
   return Object.values(value) as any;
 }
